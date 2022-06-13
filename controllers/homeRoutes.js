b241892241b93aac53687004a83a82e7b27574fa
IonicCraze <daviddeitz97@gmail.com>
2022-06-13 15:46:35 -0400

    added posting

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name']
+        }
+      ]
+
+    })
+    const posts = postData.map((posts) => posts.get({plain: true}))
+    res.render('homepage', {
+      posts, 
+      logged_in: req.session.logged_in
+    })
+    console.log(posts);
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
 })
 
 router.get('/signup', (req, res) => {
@@ -13,6 +32,10 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login');
 });
+
+router.get('/post', (req, res) => {
+  res.render('post')
+})
 
 
 
