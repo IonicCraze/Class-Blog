ebef2aab236e028a3ea1d4355dadb21bc60cea2a
IonicCraze <daviddeitz97@gmail.com>
2022-06-13 14:24:25 -0400

    added user account creation and login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+
+router.post('/newuser', async (req, res) => {
+  try {
+    const newAccount = await User.create(req.body)
+    res.status(200).json(newAccount);
+  } catch(err) {
+    res.status(400).json(err)
+
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { name: req.body.name } });
 
     if (!userData) {
       res
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,6 +6,14 @@ router.get('/', (req, res) => {
   res.render('homepage');
 })
 
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
 
 
 // router.get('/', async (req, res) => {
